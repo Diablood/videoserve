643dd67e901de4cc5e592f3fcd240232c0f4fb81
Diablood <diablood@free.fr>
2016-09-06 00:10:12 +0200

    Change header

--- web/index.php
+++ web/index.php
@@ -1,6 +1,8 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Methods: GET,POST,HEAD,PUT,OPTIONS');
 
 $loader = require_once __DIR__.'/../vendor/autoload.php';
 
