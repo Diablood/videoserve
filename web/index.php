21e428516776a33861f272ffc58038b100708a77
Diablood <diablood@free.fr>
2016-09-06 13:49:16 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -3,6 +3,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Allow-Methods: GET,POST,HEAD,PUT,OPTIONS');
+header('Allow: GET,POST,HEAD,PUT,OPTIONS');
 
 $loader = require_once __DIR__.'/../vendor/autoload.php';
 
