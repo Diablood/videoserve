1d886a43b343de3bbc9f2787ff7581a0b607bf6f
Diablood <diablood@free.fr>
2016-10-31 19:33:56 +0100

    Changement structure BDD

--- app/routes.php
+++ app/routes.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 $app->get('/api/films', function () use ($app) {
 
 	$responseData = array();
-	$sql = "SELECT s.*, c.nom, c.description FROM jos_diabwbs_serie AS s INNER JOIN serie_cat AS c WHERE s.nom_type=c.id ORDER BY titre";
+	$sql = "SELECT s.*, c.nom, c.description, c.etat as state FROM series AS s INNER JOIN categories AS c WHERE s.categorie=c.id ORDER BY titre";
 	$result = $app['db']->fetchAll($sql);
 
 	$films = array();
@@ -22,18 +22,17 @@ $app->get('/api/films', function () use ($app) {
 	foreach ($films as $film) {
 		$responseData[] = array(
 			'id' => $film['id'],
-			'state' => $film['state'],
+			'id_mdb' => $film['state'],
 			'titre' => $film['titre'],
-			'nom_type' => $film['nom_type'],
 			'saison' => $film['saison'],
 			'episode' => $film['episode'],
-			'epmax' => $film['epmax'],
+			'data' => $film['data'],
 			'etat' => $film['etat'],
-			'img_av' => $film['img_av'],
-			'url' => $film['url'],
-			'last_update' => $film['last_update'],
+			'categorie' => $film['categorie'],
 			'nom' => $film['nom'],
-			'description' => $film['description']
+			'description' => $film['description'],
+			'state' => $film['state'],
+			'last_update' => $film['last_update'],
 		);
 	}
 
@@ -48,8 +47,7 @@ $app->get('/api/films', function () use ($app) {
 $app->get('/api/films/type/{type}', function ($type) use ($app) {
 
 	$responseData = array();
-	$sql = "SELECT s.*, c.nom, c.description FROM jos_diabwbs_serie AS s INNER JOIN serie_cat AS c WHERE s.nom_type=c.id AND c.id=? ORDER BY titre";
-	// $sql = "SELECT * FROM jos_diabwbs_serie WHERE nom_type=? ORDER BY titre";
+	$sql = "SELECT s.*, c.nom, c.description, c.etat as state FROM series AS s INNER JOIN categories AS c WHERE s.categorie=c.id AND c.id=? ORDER BY titre";
 	$result = $app['db']->fetchAll($sql, array($type));
 
 	$films = array();
@@ -61,18 +59,17 @@ $app->get('/api/films/type/{type}', function ($type) use ($app) {
 	foreach ($films as $film) {
 		$responseData[] = array(
 			'id' => $film['id'],
-			'state' => $film['state'],
+			'id_mdb' => $film['id_mdb'],
 			'titre' => $film['titre'],
-			'nom_type' => $film['nom_type'],
 			'saison' => $film['saison'],
 			'episode' => $film['episode'],
-			'epmax' => $film['epmax'],
+			'data' => $film['data'],
 			'etat' => $film['etat'],
-			'img_av' => $film['img_av'],
-			'url' => $film['url'],
-			'last_update' => $film['last_update'],
+			'categorie' => $film['categorie'],
 			'nom' => $film['nom'],
-			'description' => $film['description']
+			'description' => $film['description'],
+			'state' => $film['state'],
+			'last_update' => $film['last_update'],
 		);
 	}
 
@@ -85,7 +82,7 @@ $app->get('/api/films/type/{type}', function ($type) use ($app) {
  *
  */
 $app->get('/api/films/{id}', function ($id, Request $request) use ($app) {
-	$sql = "SELECT s.*, c.nom, c.description FROM jos_diabwbs_serie AS s INNER JOIN serie_cat AS c WHERE s.id=?";
+	$sql = "SELECT s.*, c.nom, c.description, c.etat as state FROM series AS s INNER JOIN categories AS c WHERE s.id=?";
 	$row = $app['db']->fetchAssoc($sql, array($id));
 
 	if ($row) {
@@ -99,18 +96,17 @@ $app->get('/api/films/{id}', function ($id, Request $request) use ($app) {
 
 	$responseData = array(
 		'id' => $film['id'],
-		'state' => $film['state'],
+		'id_mdb' => $film['id_mdb'],
 		'titre' => $film['titre'],
-		'nom_type' => $film['nom_type'],
 		'saison' => $film['saison'],
 		'episode' => $film['episode'],
-		'epmax' => $film['epmax'],
+		'data' => $film['data'],
 		'etat' => $film['etat'],
-		'img_av' => $film['img_av'],
-		'url' => $film['url'],
-		'last_update' => $film['last_update'],
+		'categorie' => $film['categorie'],
 		'nom' => $film['nom'],
-		'description' => $film['description']
+		'description' => $film['description'],
+		'state' => $film['state'],
+		'last_update' => $film['last_update'],
 	);
 
 	return $app->json($responseData);
@@ -123,7 +119,7 @@ $app->get('/api/films/{id}', function ($id, Request $request) use ($app) {
  */
 $app->get('/api/logs', function () use ($app) {
 	$responseData = array();
-	$sql = "SELECT h.id, h.last_update, h.action, h.saison, h.episode, h.detail, s.titre, s.img_av FROM history AS h INNER JOIN jos_diabwbs_serie AS s WHERE h.id_serie=s.id ORDER BY last_update";
+	$sql = "SELECT h.id, h.last_update, h.action, h.saison, h.episode, h.detail, s.titre FROM history AS h INNER JOIN series AS s WHERE h.id_serie=s.id ORDER BY last_update";
 	$result = $app['db']->fetchAll($sql);
 
 	$logs = array();
@@ -141,7 +137,6 @@ $app->get('/api/logs', function () use ($app) {
 			'saison' => $log['saison'],
 			'episode' => $log['episode'],
 			'detail' => json_decode($log['detail']),
-			'img_av' => $log['img_av'],
 		);
 	}
 
@@ -155,7 +150,7 @@ $app->get('/api/logs', function () use ($app) {
  */
 $app->get('/api/logs/type/{type}', function ($type) use ($app) {
 	$responseData = array();
-	$sql = "SELECT h.id, h.last_update, h.action, h.saison, h.episode, h.detail, s.titre, s.img_av FROM history AS h INNER JOIN jos_diabwbs_serie AS s INNER JOIN serie_cat AS c WHERE h.id_serie=s.id AND s.nom_type=c.id AND c.id=?  ORDER BY last_update";
+	$sql = "SELECT h.id, h.last_update, h.action, h.saison, h.episode, h.detail, s.titre FROM history AS h INNER JOIN series AS s INNER JOIN categories AS c WHERE h.id_serie=s.id AND s.categorie=c.id AND c.id=?  ORDER BY last_update";
 	$result = $app['db']->fetchAll($sql, array($type));
 
 	$logs = array();
@@ -173,7 +168,6 @@ $app->get('/api/logs/type/{type}', function ($type) use ($app) {
 			'saison' => $log['saison'],
 			'episode' => $log['episode'],
 			'detail' => json_decode($log['detail']),
-			'img_av' => $log['img_av'],
 		);
 	}
 
@@ -187,7 +181,7 @@ $app->get('/api/logs/type/{type}', function ($type) use ($app) {
  */
 $app->get('/api/logs/{id}', function ($id) use ($app) {
 	$responseData = array();
-	$sql = "SELECT h.id, h.last_update, h.action, h.saison, h.episode, h.detail, s.titre, s.img_av FROM history AS h INNER JOIN jos_diabwbs_serie AS s WHERE h.id_serie=s.id AND s.id=?  ORDER BY last_update";
+	$sql = "SELECT h.id, h.last_update, h.action, h.saison, h.episode, h.detail, s.titre FROM history AS h INNER JOIN series AS s WHERE h.id_serie=s.id AND s.id=?  ORDER BY last_update";
 	$result = $app['db']->fetchAll($sql, array($id));
 
 	$logs = array();
@@ -205,7 +199,6 @@ $app->get('/api/logs/{id}', function ($id) use ($app) {
 			'saison' => $log['saison'],
 			'episode' => $log['episode'],
 			'detail' => json_decode($log['detail']),
-			'img_av' => $log['img_av'],
 		);
 	}
 
@@ -224,21 +217,15 @@ $app->post('/api/films', function (Request $request) use ($app) {
 	if (!$request->request->has('titre')) {
 		return $app->json('Paramètre manquant: titre', 400);
 	}
-	if (!$request->request->has('nom_type')) {
-		return $app->json('Paramètre manquant: nom_type', 400);
-	}
-	if (!$request->request->has('etat')) {
-		return $app->json('Paramètre manquant: etat', 400);
-	}
 
 	$film = array();
-	$film['state'] = 1;
-	$film['ordering'] = $app['db']->lastInsertId();
-	$film['checked_out'] = 0;
-	$film['checked_out_time'] = date_create()->format('Y-m-d H:i:s');
-	$film['created_by'] = 438;
+
+	if ($request->request->has('id_mdb')) {
+		$film['id_mdb'] = $request->request->get('id_mdb');
+	} else {
+		$film['id_mdb'] = null;
+	}
 	$film['titre'] = $request->request->get('titre');
-	$film['nom_type'] = $request->request->get('nom_type');
 	if ($request->request->has('saison')) {
 		$film['saison'] = $request->request->get('saison');
 	} else {
@@ -247,59 +234,53 @@ $app->post('/api/films', function (Request $request) use ($app) {
 	if ($request->request->has('episode')) {
 		$film['episode'] = $request->request->get('episode');
 	} else {
-		$film['episode'] = 0;
+		$film['episode'] = 1;
 	}
-	if ($request->request->has('epmax')) {
-		$film['epmax'] = $request->request->get('epmax');
+	if ($request->request->has('data')) {
+		$film['data'] = $request->request->get('data');
 	} else {
-		$film['epmax'] = 0;
+		$film['data'] = "";
 	}
-	$film['etat'] = $request->request->get('etat');
-	if ($request->request->has('img_av')) {
-		$film['img_av'] = $request->request->get('img_av');
+	if ($request->request->has('etat')) {
+		$film['etat'] = $request->request->get('etat');
 	} else {
-		$film['img_av'] = "/images/avatar/aucune.jpg";
+		$film['etat'] = 1;
 	}
-	if ($request->request->has('url')) {
-		$film['url'] = $request->request->get('url');
+	if ($request->request->has('categorie')) {
+		$film['categorie'] = $request->request->get('categorie');
 	} else {
-		$film['url'] = "";
+		$film['categorie'] = 1;
 	}
+
+
+
 	$film['last_update'] = date_create()->format('Y-m-d H:i:s');
 
 	$filmData = array(
-		'state' => $film['state'],
-		'ordering' => $film['ordering'],
-		'checked_out' => $film['checked_out'],
-		'checked_out_time' => $film['checked_out_time'],
-		'created_by' => $film['created_by'],
+		'id_mdb' => $film['id_mdb'],
 		'titre' => $film['titre'],
-		'nom_type' => $film['nom_type'],
 		'saison' => $film['saison'],
 		'episode' => $film['episode'],
-		'epmax' => $film['epmax'],
+		'data' => $film['data'],
 		'etat' => $film['etat'],
-		'img_av' => $film['img_av'],
-		'url' => $film['url'],
-		'last_update' => $film['last_update']
+		'categorie' => $film['categorie'],
+		'last_update' => $film['last_update'],
 	);
 
-	$app['db']->insert('jos_diabwbs_serie', $filmData);
+	$app['db']->insert('series', $filmData);
 	$id = $app['db']->lastInsertId();
-	$app['db']->update('jos_diabwbs_serie', array('ordering' => $id), array('id' => $id));
+	// $app['db']->update('series', array('id' => $id));
 
 	$responseData = array(
 		'id' => $id,
-		'state' => $film['state'],
+		'id_mdb' => $film['id_mdb'],
 		'titre' => $film['titre'],
-		'nom_type' => $film['nom_type'],
 		'saison' => $film['saison'],
 		'episode' => $film['episode'],
-		'epmax' => $film['epmax'],
+		'data' => $film['data'],
 		'etat' => $film['etat'],
-		'img_av' => $film['img_av'],
-		'url' => $film['url'],
-		'last_update' => $film['last_update']
+		'categorie' => $film['categorie'],
+		'last_update' => $film['last_update'],
 	);
 
 	// LOG
@@ -322,7 +303,7 @@ $app->post('/api/films', function (Request $request) use ($app) {
  *
  */
 $app->delete('/api/films/{id}', function ($id, Request $request) use ($app) {
-	// $app['db']->delete('jos_diabwbs_serie', array('id' => $id));
+	// $app['db']->delete('series', array('id' => $id));
 
 	return $app->json('No content', 204);
 })->bind('api_film_delete');
@@ -336,7 +317,7 @@ $app->put('/api/films/{id}', function ($id, Request $request) use ($app) {
 
 	$log = array();
 
-	$sql = "SELECT * FROM jos_diabwbs_serie WHERE id=?";
+	$sql = "SELECT * FROM series WHERE id=?";
 	$row = $app['db']->fetchAssoc($sql, array($id));
 
 	if ($row) {
@@ -349,60 +330,44 @@ $app->put('/api/films/{id}', function ($id, Request $request) use ($app) {
 		$app->abort(404, "Ce film n'existe pas");
 	}
 
-	if ($request->request->has('state')) {
-		$film['state'] = $request->request->get('state');
-	}
-	if($film['checked_out_time'] == "0000-00-00 00:00:00") {
-		$film['checked_out_time'] = date_create()->format('Y-m-d H:i:s');
+	if ($request->request->has('id_mdb')) {
+		$film['id_mdb'] = $request->request->get('id_mdb');
 	}
 	if ($request->request->has('titre')) {
-		if($film['titre'] != $request->request->get('titre')) 	$log['titre'] = $film['titre'];
+		if($film['titre'] != $request->request->get('titre'))		$log['titre'] = $film['titre'];
 		$film['titre'] = $request->request->get('titre');
 	}
-	if ($request->request->has('nom_type')) {
-		if($film['nom_type'] != $request->request->get('nom_type')) 	$log['nom_type'] = $film['nom_type'];
-		$film['nom_type'] = $request->request->get('nom_type');
-	}
 	if ($request->request->has('saison')) {
-		if($film['saison'] != $request->request->get('saison')) 	$log['saison'] = $film['saison'];
+		if($film['saison'] != $request->request->get('saison'))		$log['saison'] = $film['saison'];
 		$film['saison'] = $request->request->get('saison');
 	}
 	if ($request->request->has('episode')) {
-		if($film['episode'] != $request->request->get('episode')) 	$log['episode'] = $film['episode'];
+		if($film['episode'] != $request->request->get('episode'))	$log['episode'] = $film['episode'];
 		$film['episode'] = $request->request->get('episode');
 	}
-	if ($request->request->has('epmax')) {
-		$film['epmax'] = $request->request->get('epmax');
+	if ($request->request->has('data')) {
+		$film['data'] = $request->request->get('data');
 	}
 	if ($request->request->has('etat')) {
+		if($film['etat'] != $request->request->get('etat'))			$log['etat'] = $film['etat'];
 		$film['etat'] = $request->request->get('etat');
 	}
-	if ($request->request->has('img_av')) {
-		if($film['img_av'] != $request->request->get('img_av')) 	$log['img_av'] = $film['img_av'];
-		if($request->request->get('img_av') == "") {
-			$film['img_av'] = "/images/avatar/aucune.jpg";
-		} else {
-			$film['img_av'] = $request->request->get('img_av');
-		}
-	}
-	if ($request->request->has('url')) {
-		$film['url'] = $request->request->get('url');
+	if ($request->request->has('categorie')) {
+		$film['categorie'] = $request->request->get('categorie');
 	}
 	$film['last_update'] = date_create()->format('Y-m-d H:i:s');
 
-	$app['db']->update('jos_diabwbs_serie', $film, array('id' => $id));
+	$app['db']->update('series', $film, array('id' => $id));
 
 	$responseData = array(
 		'id' => $id,
-		'state' => $film['state'],
+		'id_mdb' => $film['id_mdb'],
 		'titre' => $film['titre'],
-		'nom_type' => $film['nom_type'],
 		'saison' => $film['saison'],
 		'episode' => $film['episode'],
-		'epmax' => $film['epmax'],
+		'data' => $film['data'],
 		'etat' => $film['etat'],
-		'img_av' => $film['img_av'],
-		'url' => $film['url'],
+		'categorie' => $film['categorie'],
 		'last_update' => $film['last_update'],
 		'log' => json_encode($log)
 	);
