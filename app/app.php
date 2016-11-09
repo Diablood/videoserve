df567b967bfe9c040eea0ad19f07f7f56acc9021
Nounzor <nounzorus@gmail.com>
2016-11-09 13:51:14 +0100

    Merge config

--- app/app.php
+++ app/app.php
@@ -18,5 +18,5 @@ $app->before(function (Request $request) {
 
 $app->after(function (Request $request, Response $response) {
     $response->headers->set('Access-Control-Allow-Origin', '*');
-    $response->headers->set('Access-Control-Allow-Methods','GET,POST,HEAD,DELETE,PUT,OPTIONS');
+    $response->headers->set('Access-Control-Allow-Methods','GET,POST,HEAD,DELETE,PUT,PATCH,OPTIONS');
 });
