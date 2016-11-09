2724f7412fb1111bce9d4104e11d7e09adba4f86
Nounzor <nounzorus@gmail.com>
2016-11-09 13:43:19 +0100

    Options

--- app/config/dev.php
+++ app/config/dev.php
@@ -2,13 +2,10 @@
 
 // Doctrine (db)
 $app['db.options'] = array(
-    'dbname' => 'diablood',
+    'dbname' => 'diabloodwebsite',
     'user' => 'diablood',
     'password' => 'password',
-    'host' => '127.0.0.1',
+    'host' => 'localhost',
     'driver' => 'pdo_mysql',
   	'charset'  => 'utf8',
 );
-
-// enable the debug mode
-$app['debug'] = true;
